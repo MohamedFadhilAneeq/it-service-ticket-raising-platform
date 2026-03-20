7bdd690d531bb088ad465017868f6d44e0e07841
Mohamed Fadhil Aneeq H <155130448+MohamedFadhilAneeq@users.noreply.github.com>
2026-03-20 19:12:09 +0530

    Update server.js

--- it-service-backend/server.js
+++ it-service-backend/server.js
@@ -27,7 +27,7 @@ const mongoose = require("mongoose");
 const { trainClassifier, classifyIssue } = require("./ai/ticketClassifier");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 /* Train AI model when server starts */
 trainClassifier();
